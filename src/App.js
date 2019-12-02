6729f90219ff5d3e86131f52450f173c9d1e72ed
kaito yokohama <sk.kaito.yokohama@gmail.com>
2019-12-02 14:47:06 +0900

    git component

--- src/App.js
+++ src/App.js
@@ -3,16 +3,36 @@ import React, {
 } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onChange={()=> {console.log("I am clicked")}}/>
-      </React.Fragment>
-    )
-  }
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//       <label htmlFor="bar">bar</label>
+//       <input type="text" onChange={()=> {console.log("I am clicked")}}/>
+//       </React.Fragment>
+//     )
+//   }
+// }
+
+const App = ()=>{
+
+  return (
+  <div>
+    
+    <Cat />
+    <Cat />
+    <Cat />
+    <Cat />
+    <Cat />
+
+
+  </div>
+  )
 }
 
+
+const Cat= ()=>{
+  return <div> Mwlow</div>
+}
 
 export default App;
