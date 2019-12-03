3b7d08682347732aebc3805525f0fc5c30bf78bf
kaito yokohama <sk.kaito.yokohama@gmail.com>
2019-12-03 17:09:48 +0900

    implemtnt events index

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,14 +1,16 @@
-import {INCREMENT, DECREMENT} from '../actions'
-
-const initialState = {value :0}
-
-export default(state = initialState,action)=>{
+import _ from 'lodash'
+import {READ_EVENTS} from '../actions'
+ 
+export default(events = {},action)=>{
   switch(action.type){
-    case INCREMENT:
-        return {value: state.value +1}
-    case DECREMENT:
-        return {value: state.value -1}
+    case READ_EVENTS:
+
+    //[
+      //{"id":1,"title":"Let's have an event 1!","body":"This is the body for event 1."},
+      //{"id":2,"title":"Let's have an event 2!","body":"This is the body for event 2."}
+    //]
+      return _.mapKeys(action. responese.data, 'id')
     default:
-        return  state
+        return  events
   }
 }
