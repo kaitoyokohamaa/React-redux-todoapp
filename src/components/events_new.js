8b556d95aa0105c7bcd6cc2c9cf7c34a81d7febc
kaito yokohama <sk.kaito.yokohama@gmail.com>
2019-12-03 21:13:51 +0900

    update

--- src/components/events_new.js
+++ src/components/events_new.js
@@ -26,13 +26,13 @@ async onSubmit(values){
 }
 
 render (){
-  const {handleSubmit} = this.props
+  const {handleSubmit, pristine,submitting} = this.props
     return (
       <form onSubmit={handleSubmit(this.onSubmit)}>
         <div><Field label ="Title" name="title" type="text" component={this.renderFireld} /></div>
         <div><Field label ="Body" name="body" type="text" component={this.renderFireld }/> </div>
          <div>
-           <input type ="submit" value="Submit" disabled={false}/>
+           <input type ="submit" value="Submit" disabled={pristine ||submitting }/>
            <Link to ="/" >Cancel</Link>
          </div>
      </form>
