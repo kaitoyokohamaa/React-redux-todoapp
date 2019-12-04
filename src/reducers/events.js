24dfa8bb1db4e5cdbbe54b52284eb817bf6a039e
kaito yokohama <sk.kaito.yokohama@gmail.com>
2019-12-04 16:07:59 +0900

    implement delete feature

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,16 +1,22 @@
 import _ from 'lodash'
-import {READ_EVENTS} from '../actions'
- 
-export default(events = {},action)=>{
-  switch(action.type){
+import {
+  READ_EVENTS,
+  DELETE_EVENT,
+} from '../actions'
+
+export default (events = {}, action) => {
+  switch (action.type) {
     case READ_EVENTS:
 
-    //[
+      //[
       //{"id":1,"title":"Let's have an event 1!","body":"This is the body for event 1."},
       //{"id":2,"title":"Let's have an event 2!","body":"This is the body for event 2."}
-    //]
-      return _.mapKeys(action. responese.data, 'id')
+      //]
+      return _.mapKeys(action.responese.data, 'id')
+    case DELETE_EVENT:
+      delete events[action.id]
+      return {...events}
     default:
-        return  events
+      return events
   }
 }
